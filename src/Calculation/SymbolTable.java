6216293b42a5a720d008e499eb7327e2b6a6606c
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-04-18 11:53:43 +0700

    Update SymbolTable.java

--- src/Calculation/SymbolTable.java
+++ src/Calculation/SymbolTable.java
@@ -116,14 +116,49 @@ public class SymbolTable{
     SymbolTable(){
 		Panel.setBounds(10,400,1440,600);
 		Panel.setLayout(new GridLayout(7,5,1,1));
-
-        for (JButton button : numButton.Buttons){
-            Panel.add(button);
-        }
-        
-        for (JButton button : operButton.Buttons){
-            Panel.add(button);
-        }
+	    
+	    
+		Panel.add(operButton.ln);
+		Panel.add(operButton.log);
+		Panel.add(operButton.pow10);
+		Panel.add(operButton.del);
+		Panel.add(operButton.clr);
+		
+		Panel.add(operButton.fac);
+		Panel.add(operButton.pi);
+		Panel.add(operButton.e);
+		Panel.add(operButton.round);
+		Panel.add(operButton.mod);
+		
+		Panel.add(operButton.sin);
+		Panel.add(operButton.cos);
+		Panel.add(operButton.tan);
+		Panel.add(operButton.cot);
+		Panel.add(operButton.div);
+		
+		Panel.add(operButton.pow);
+		Panel.add(numButton.Buttons[7]);
+		Panel.add(numButton.Buttons[8]);
+		Panel.add(numButton.Buttons[9]);
+		Panel.add(operButton.mul);
+		
+		Panel.add(operButton.sqr);
+		Panel.add(numButton.Buttons[4]);
+		Panel.add(numButton.Buttons[5]);
+		Panel.add(numButton.Buttons[6]);
+		Panel.add(operButton.sub);
+		
+		Panel.add(operButton.sqrt);
+		Panel.add(numButton.Buttons[1]);
+		Panel.add(numButton.Buttons[2]);
+		Panel.add(numButton.Buttons[3]);
+		Panel.add(operButton.add);
+		
+		Panel.add(operButton.abs);
+		Panel.add(operButton.neg);
+		Panel.add(numButton.Buttons[0]);
+		Panel.add(operButton.dot);
+		Panel.add(operButton.equ);
     }
 
 }
